1353adb3a75e10ee0981f6dd1d1d20e28122dd15
Bumblebash <ssimbwabashir777@gmail.com>
2025-10-03 21:37:45 +0200

    Learnt about date functions

---
+++ Microsoft/Datefunctions.sql
@@ -0,0 +1,28 @@
+USE PRACTICE;
+
+SELECT * FROM messages_table;
+
+/* renaming A table */
+/*sp stands for stored procedures */
+EXEC sp_rename 'messages', 'messages_table';
+
+/** Changing the Data type **/
+alter table messages_table alter column sent_date DATETIME2(0);
+
+/*Write a query to identify the top 2 Power Users who sent the highest number of messages
+on Microsoft Teams in August 2022.
+Display the IDs of these 2 users along with the total number of messages they sent. 
+Output the results in descending order based on the count of the messages.*/
+
+SELECT TOP 2(sender_id), COUNT(content) As Total_messages_sent FROM messages_table
+				WHERE YEAR(sent_date) = 2022
+				AND MONTH(sent_date) = 8
+				GROUP BY sender_id
+				ORDER BY Total_messages_sent DESC;
+
+/*Option 2 for Mysql */
+SELECT sender_id, COUNT(Content) As Total_messages  FROM messages_table
+WHERE EXTRACT(YEAR FROM sent_date) = 2022
+AND EXTRACT(MONTH FROM sent_date) = 8
+GROUP BY sender_id
+ORDER BY  Total_messages DESC LIMIT 2;
