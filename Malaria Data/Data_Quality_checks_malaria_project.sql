ea55a778fe2ccf66acfcf2c0cec56360d340dcca
Bumblebash <ssimbwabashir777@gmail.com>
2025-12-24 16:54:21 +0200

    Added scripts for Data Qulaity checks for malaria project

--- Malaria Data/Data_Quality_checks_malaria_project.sql
+++ Malaria Data/Data_Quality_checks_malaria_project.sql
@@ -42,5 +42,41 @@ SELECT Region, COUNT(District) As total_Districts
         ,'Kole District', 'Kwania District', 'Lira District', 'Otuke District', 'Oyam District'
         )
         ;
+--Confirming for Masaka City
+ SELECT SUM(TotalPopulation) AS Totalpn
+           FROM malaria_data
+           WHERE YEAR = 2024
+           AND District = 'Masaka City';
 
- 
+---Total Cases Confirmed By R/D and BS for South Central
+SELECT  Region , SUM(TotalCasesConfirmed_BSRDT) as TotalCASES 
+FROM malaria_data
+    WHERE Region = District AND Region = 'South Central' AND YEAR = 2024
+    GROUP BY Region;
+
+    ----Confirming Total Population without masaka City
+    SELECT District,  TotalPopulation
+    FROM malaria_data 
+    WHERE District = 'Masaka City' AND Year = '2020'
+   ;
+
+
+
+
+
+--Confirming Total Cases for Masaka City 
+SELECT  TotalCasesConfirmed_BSRDT
+           FROM malaria_data
+           WHERE YEAR = 2020
+           AND District = 'Masaka City';
+
+
+SELECT  AVG(IncidenceRate) AS avg_incidence FROM malaria_data
+
+WHERE Year = '2024'
+
+
+;
+
+
+
