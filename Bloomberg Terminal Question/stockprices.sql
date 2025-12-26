2df8ace9ec2ccb522944a757533e79d4854337e4
Bumblebash <ssimbwabashir777@gmail.com>
2025-12-27 01:30:38 +0200

    Practiced Year Over Year Growth for the Bloomberg question

--- Bloomberg Terminal Question/stockprices.sql
+++ Bloomberg Terminal Question/stockprices.sql
@@ -24,11 +24,19 @@ SELECT date, ticker, closing_price,
 			PARTITION BY ticker ORDER BY date	
 			), 0) As Previous_close_price
 FROM stockprice
-)
+WHERE YEAR(date) = 2023
+),
 --SELECT  * FROM previous_cte;
-
+difference2 AS (
 SELECT date, ticker , closing_price, Previous_close_price, closing_price - Previous_close_price As difference_
-FROM previous_cte;
+FROM previous_cte
+)
+Select date, ticker, closing_price, Previous_close_price, difference_,  
+--(difference_ / Previous_close_price) * 100 AS Y_O_Y_growth from difference2;
+		CASE WHEN Previous_close_price = 0 THEN 0
+		ELSE (difference_ / Previous_close_price) * 100
+		END AS Y_O_Y_growth
+FROM difference2;
 
 
 --2. Calculate the difference between the closing price of the current month and the closing price from 3 months prior.
@@ -43,3 +51,13 @@ SELECT  date, ticker, closing_price,
 
 SELECT date , closing_price ,previous_closing_month_price, closing_price - previous_closing_month_price AS Diiference_in_px
  FROM previous_month_px
+
+
+ SELECT
+  date,
+  closing_price,
+  LEAD(closing_price) OVER (ORDER BY date) AS next_month_close,
+  LEAD(closing_price) OVER (ORDER BY date) - closing_price as difference
+FROM stockprice
+WHERE YEAR(date) = 2023
+  AND ticker = 'GOOG';
